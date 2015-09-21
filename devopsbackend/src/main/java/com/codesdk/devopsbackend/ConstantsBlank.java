cc9c0a55352cb1e251561b78ca735e40c02508b6
Code SDK <codesdktutorials@gmail.com>
2015-09-21 02:40:14 -0400

    Corrected name of ConstantsBlank

--- devopsbackend/src/main/java/com/codesdk/devopsbackend/ConstantsBlank.java
+++ devopsbackend/src/main/java/com/codesdk/devopsbackend/ConstantsBlank.java
@@ -3,7 +3,7 @@ package com.codesdk.devopsbackend;
 /**
  * Contains the client IDs and scopes for allowed clients consuming the helloworld API.
  */
-public class Constants {
+public class ConstantsBak {
   public static final String WEB_CLIENT_ID = "replace this with your web client ID";
   public static final String ANDROID_CLIENT_ID = "replace this with your Android client ID";
   public static final String IOS_CLIENT_ID = "replace this with your iOS client ID";
